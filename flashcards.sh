38f4dace161e5b6c191e207f2d82e005db7f9afc
David M. Lentz <davidmlentz@gmail.com>
2024-08-02 08:24:53 -0600

    New questions and accept parameter to filter questions

--- flashcards.sh
+++ flashcards.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# TODO: accept a parameter (string) and select randomly only from questions that contain that string
-# e.g. ./flashcards.sh glacier
+# This script optionally accepts a parameter (string) to filter for questions that include that string.
+# e.g. ./flashcards.sh Agent
 
 #DATAFILE=data
 DATAFILE=ddog_data
@@ -9,20 +9,29 @@ DATAFILE=ddog_data
 ANOTHER=y
 
 while true; do
-	if [ "$ANOTHER" = "y" ]; then
-		clear
-		Qtemp=$(grep -e '^[0-9]* Q:' $DATAFILE | sort -R | head -n 1)
-		Q=$(echo $Qtemp | sed 's/^\(.*\)Q: \(.*\)$/\2/')
-		echo $Q
-		num=$(echo $Qtemp | sed 's/^\([0-9]*\)\(.*\)/\1/')
-		Atemp=$(grep -e "^$num " $DATAFILE )
-		read YOURANSWER
-		A=$(echo $Atemp | sed 's/^\(.*\)A: \(.*\)$/\2/')
-		echo $A
-		echo ""
-		echo "Another question? y/n"
-		read ANOTHER
-	else
-		exit
-	fi
+    if [ "$ANOTHER" = "y" ]; then
+        clear
+        Qtemp=$(grep -e '^[0-9]* Q:' $DATAFILE | sort -R | head -n 1)
+	Q=$(echo $Qtemp | sed 's/^\(.*\)Q: \(.*\)$/\2/')
+
+        if [[ -n $1 ]]; then
+             # We received a string to use as a filter and show only questions that match
+             echo "$Q" | grep -i "$1" > /dev/null
+             if [ $? -eq 1 ]; then
+               continue
+             fi
+        fi
+
+        echo $Q
+        num=$(echo $Qtemp | sed 's/^\([0-9]*\)\(.*\)/\1/')
+	Atemp=$(grep -e "^$num " $DATAFILE )
+	read YOURANSWER
+	A=$(echo $Atemp | sed 's/^\(.*\)A: \(.*\)$/\2/')
+	echo $A
+	echo ""
+	echo "Another question? y/n"
+	read ANOTHER
+    else
+        exit
+    fi
 done
